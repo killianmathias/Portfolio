bfeb900b1f6055193855b83f4ce680f04c5dc94e
Killian Mathias <killianmathias360@gmail.com>
2025-05-06 18:29:52 +0200

    Première modifications

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Je suis Killian Mathias, étudiant en informatique</p>
+        <p>Je suis Killian Mathias, étudiant en informatique à l'université de Besançon</p>
       </header>
     </div>
   );
